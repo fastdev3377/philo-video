bad929dab4c37b88a12f7b984e1435ea6ff63419
fastdev3377 <bluesea20190@gmail.com>
2024-02-28 08:37:08 -0500

    update guide view page for displaying schedule data and favorite state

--- src/components/guide/guide.js
+++ src/components/guide/guide.js
@@ -1,12 +1,26 @@
 import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { calculateLiveMarkerPosition, formatHour, generateGuideTimes } from '../../utils';
+import {
+  calcHeightByDuration,
+  calculateLiveMarkerPosition,
+  formatHour,
+  formatTime,
+  generateGuideTimes,
+} from '../../utils';
+
+import { getScheduleData } from '../../redux/selectors/schedule';
+import { updateFavorite } from '../../redux/actions/favorite';
 
 import styles from './guide.module.sass';
+import { getFavoritesData } from '../../redux/selectors/favorite';
 
 export function Guide() {
+  const dispatch = useDispatch();
   const guideElementRef = useRef(null);
   const [guideElement, setGuideElement] = useState(guideElementRef.current);
+  const scheduleData = useSelector(getScheduleData);
+  const favoriteData = useSelector(getFavoritesData);
 
   const liveMarkerPosition = calculateLiveMarkerPosition();
   const livePosition = guideElement ? guideElement.scrollHeight * liveMarkerPosition : 0;
@@ -30,6 +44,10 @@ export function Guide() {
     }
   }, [guideElement]);
 
+  const handleUpdateFavorite = ({ id, value }) => {
+    dispatch(updateFavorite({ id, value }));
+  };
+
   const renderTimeline = () => {
     const guideHours = generateGuideTimes().map(date => formatHour(date));
 
@@ -50,7 +68,42 @@ export function Guide() {
           <div className={styles.liveMarker} style={liveMarkerStyle} />
           {renderTimeline()}
         </div>
-        <div className={styles.presentations}>{/* scheduleData */}</div>
+        <div className={styles.presentations}>
+          {scheduleData.map(({ startTime, endTime, video }, idx) => {
+            const {
+              duration, id, thumb, title,
+            } = video;
+            // It seems like there are some duplicated ids in sample scheduleData.
+            // For preventing some build warnings defining new unique id with index.
+            const uniqueId = `${id}-${idx}`;
+            const isFavorite = favoriteData[uniqueId];
+
+            return (
+              <div
+                key={uniqueId}
+                className={styles.presentation}
+                style={{
+                  // set the height of presentation based on duration
+                  height: `${calcHeightByDuration(duration)}em`,
+                  // set the background image from thumbnail
+                  backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url(${thumb})`,
+                }}
+              >
+                <button
+                  type="button"
+                  className={`${styles.presentationFavorite}${
+                    isFavorite ? ` ${styles.favorite}` : ''
+                  }`}
+                  onClick={() => handleUpdateFavorite({ id: uniqueId, value: !isFavorite })}
+                >
+                  {isFavorite ? '★' : '☆'}
+                </button>
+                <span className={styles.presentationTime}>{formatTime(startTime)}</span>
+                <span className={styles.presentationTitle}>{title}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
